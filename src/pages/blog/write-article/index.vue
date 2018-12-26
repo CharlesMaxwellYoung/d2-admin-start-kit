9d9f544329696d204734749397db5138d9b4adce
Fantasy <805841483@qq.com>
2018-12-26 13:52:42 +0800

    :sparkles: 添加内容编写页面

---
+++ src/pages/blog/write-article/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="write-article">
+        <div class="write-content"></div>
+        <div class="view-content"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "writeArticle"
+    }
+</script>
+
+<style scoped>
+
+</style>
